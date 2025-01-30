b49b63597577151f6574b3b120197d94d120dc70
Joakim Jansson <joakimxjansson@gmail.com>
2025-01-30 11:21:12 +0100

    Test succeed

--- TicketBookingCore/TicketBookingRequestProcessor.cs
+++ TicketBookingCore/TicketBookingRequestProcessor.cs
@@ -1,13 +1,23 @@
 namespace TicketBookingCore;
 
 public class TicketBookingRequestProcessor {
+    private readonly ITicketBookingRepository _ticketBookingRepository;
     public TicketBookingRequestProcessor(ITicketBookingRepository ticketBookingRepository)
     {
+        _ticketBookingRepository = ticketBookingRepository;
     }
 
     public TicketBookingResponse Book(TicketBookingRequest request) {
         if (request is null)
+            
             throw new ArgumentNullException(nameof(request));
+        var ticketBooking = new TicketBooking {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email
+        };
+        _ticketBookingRepository.Save(ticketBooking);
+        
         return new TicketBookingResponse() {
             FirstName = request.FirstName,
             LastName = request.LastName,
